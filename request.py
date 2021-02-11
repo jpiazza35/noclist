904abe20c69db2d6ce462b04e402484cbc9cc037
jpiazza35 <piazzajignacio@gmail.com>
2021-02-10 22:52:24 -0500

    first commit

---
+++ request.py
@@ -0,0 +1,24 @@
+#Importing libraries 
+import requests;
+import json
+import hashlib
+from retry.api import retry_call
+
+
+#api end-points
+res = requests.get('http://0.0.0.0:8888/auth')
+print(res.headers)
+token = res.headers['Badsec-Authentication-Token']
+print(token)
+
+
+def get_checksum(token, path_to_request):
+    value = hashlib.sha256(token+path_to_request).hexdigest()
+    return value
+
+
+
+valuechecksum = get_checksum(token,'/users')
+print(valuechecksum)
+    
+
