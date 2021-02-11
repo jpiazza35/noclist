6735eb26347a17ab7aabcf42efd6fa0c3609b9f5
jose <piazzajignacio@gmail.com>
2021-02-11 01:38:00 -0300

    c

--- request.py
+++ request.py
@@ -4,23 +4,31 @@ import json
 import hashlib
 from retry.api import retry_call
 
-
-#api end-points
-res = requests.get('http://0.0.0.0:8888/auth')
-print(res.headers)
-token = res.headers['Badsec-Authentication-Token']
-print("este es el token", type(token))
-
-
 def get_checksum(token, path_to_request):
     value_to_string = token+path_to_request
     value = hashlib.sha256(value_to_string.encode('utf8')).hexdigest()
     return value
-    
 
 
+#api end-points
+baseurl = 'http://0.0.0.0:8888'
+res = requests.get(baseurl+'/auth')
+print(res.headers)
+token = res.headers['Badsec-Authentication-Token']
+print("este es el token", type(token))
 
-valuechecksum = get_checksum(token,'/users')
+path = '/users'
+valuechecksum = get_checksum(token,path)
 print(valuechecksum)
+
+##setting header    
+headers = {'X-Request-Checksum': valuechecksum}
+res = requests.get(baseurl+path)
+print(res.body)
+
     
+
+
+
+
 
